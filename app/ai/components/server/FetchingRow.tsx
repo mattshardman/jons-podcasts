c089eb569f5b199a6fec69e8fe80280293919b67
mattshardman <mattshardman@gmail.com>
2023-12-06 10:11:20 +0200

    sd

--- app/ai/components/server/FetchingRow.tsx
+++ app/ai/components/server/FetchingRow.tsx
@@ -5,13 +5,13 @@ import React from "react";
 export const Fallback = () => {
   return (
     <tr>
-      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0 capitalize">
+      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0 ">
         Loading...
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize"></td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize"></td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize"></td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize"></td>
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 "></td>
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 "></td>
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 "></td>
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 "></td>
     </tr>
   );
 };
@@ -70,44 +70,44 @@ export function FetchingRow({ input }: Props) {
 
   return (
     <tr>
-      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0 capitalize">
+      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0 ">
         {podcast.hostName.toLowerCase().includes("unknown")
           ? ""
           : podcast.hostName}
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         with {podcast.previous.guest}
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         about{" "}
         {typeof podcast.previous.keywords === "string"
           ? podcast?.previous?.keywords
           : podcast?.previous?.keywords?.join(", ")}
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         on{" "}
         {typeof podcast.current.keywords === "string"
           ? podcast?.current?.keywords
           : podcast?.current?.keywords?.join(", ")}
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         with {podcast.current.guest}
       </td>
 
       {/* original  */}
-      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         {podcast.original.recentEpisodeTitles.slice(0, 50)}
       </td>
-      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         {podcast.original.recentEpisodeBody.slice(0, 50)}
       </td>
-      <td className="w-48 truncate whitespace- px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="w-48 truncate whitespace- px-3 py-4 text-sm text-gray-300 ">
         {podcast.original.previousEpisodeTitle.slice(0, 50)}
       </td>
-      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         {podcast.original.previousEpisodeBody.slice(0, 50)}
       </td>
-      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 capitalize">
+      <td className="w-48 truncate whitespace-nowrap px-3 py-4 text-sm text-gray-300 ">
         {podcast.original.showInfo.slice(0, 50)}
       </td>
     </tr>
